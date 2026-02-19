470af0e4bea059a61ae1f51d99e90aca6e9c19fb
Emmanuel <emmanuelbaraka572@gmail.com>
2026-02-19 09:10:44 +0300

    Added bmr.js in utils directory

---
+++ fit-track-app/src/utils/bmr.js
@@ -0,0 +1,6 @@
+export function calculateBMR(age, weight, height, gender) {
+  if (gender === "male")
+    return 10*weight + 6.25*height - 5*age + 5;
+  else
+    return 10*weight + 6.25*height - 5*age - 161;
+}
